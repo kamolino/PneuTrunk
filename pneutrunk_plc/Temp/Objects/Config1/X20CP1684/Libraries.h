eacb0838ed0aaa7d4a1848a0748fc5b86854efa8
km <ivan.virgala@tuke.sk>
2024-05-06 15:19:40 +0200

    plc

--- pneutrunk_plc/Temp/Objects/Config1/X20CP1684/Libraries.h
+++ pneutrunk_plc/Temp/Objects/Config1/X20CP1684/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20240506113853_
-#define _LIBRARIES_20240506113853_
+#ifndef _LIBRARIES_20240506150232_
+#define _LIBRARIES_20240506150232_
 
 __asm__(".section \".plc\"");
 
@@ -678,5 +678,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/regulatory/regulatory.var\\\" sc
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20240506113853_ */
+#endif /* _LIBRARIES_20240506150232_ */
 
