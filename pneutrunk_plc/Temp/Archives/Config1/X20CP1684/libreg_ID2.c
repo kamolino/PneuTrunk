07014ce4e2a824925e95d5d69ca82a1060968723
km <ivan.virgala@tuke.sk>
2024-05-14 19:33:19 +0200

    plc

---
+++ pneutrunk_plc/Temp/Archives/Config1/X20CP1684/libreg_ID2.c
@@ -0,0 +1,2 @@
+void FF_PID_r_t_ID2(void) {};
+void _FF_PID_r_t_ID2(void) {};
---
+++ pneutrunk_plc/Temp/Includes/Source/MATLAB_includes/R2021b/AsDefault.h
@@ -0,0 +1,6 @@
+#ifndef _SOURCE_MATLAB_INCLUDES_R2021B_DEFAULT_212187001
+#define _SOURCE_MATLAB_INCLUDES_R2021B_DEFAULT_212187001
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#endif
